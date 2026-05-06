71edb9a77b22781b8e84ce4204d4433a26960c0d
SlashYolo <slashyprod@yandex.ru>
2026-05-06 15:16:47 +0300

    COM-port logger + тестирование на записанных данных
    - добавлен comport_logger.py с встроенным потоковым асинхронным чтением данных с COM порта, а также с блоком чтения данных из файла
    - добавлен бинарный файл с данными
    
    Co-Authored-By: Kolonard <35596834+Kolonard@users.noreply.github.com>

---
+++ COMPORT_RS485/comport_logger.py
@@ -0,0 +1,160 @@
+# Скрипт для асинхронной записи потока с COM-port
+# В начале есть проверка чтения по выбранному протоколу из .bin файла, записанного предварительно
+# (Активно используется)
+
+import asyncio
+from socketserver import TCPServer
+
+import serial_asyncio
+import struct
+import time
+
+from urllib3.response import GzipDecoder
+
+PORT = "COM3"
+BAUDRATE = 921600
+# BAUDRATE = 115200 # для теста на Arduino UNO (работает, avg Rate: 188 pkt/s, avg Loss: 0
+
+SYNC = b'\xAA\x55'
+FRAME_SIZE  = 32
+#FRAME_SIZE += 2 # 32+2
+
+def crc16(data):
+    crc = 0xFFFF
+    for b in data:
+        crc ^= b
+        for _ in range(8):
+            if crc & 1:
+                crc = (crc >> 1) ^ 0xA001
+            else:
+                crc >>= 1
+    return crc
+
+# закомментировать при реальном подключении
+with open("bindata/IMU500_static.bin", "rb") as f:
+    while True:
+        chunk = f.read(FRAME_SIZE)
+        if len(chunk) < FRAME_SIZE:
+            break
+        payload = chunk # остается только здесь для примера
+
+        # под протокол БЧЭ-500 режимов 2-3
+        # нумерация байтов 0:31
+        header  = struct.unpack('<H', payload[:2])[0]  # не уверен с < или >
+        # чтение векторов G и A через '<f' идет с nan, выбран '<i' согласно документации
+        Gx      = struct.unpack('<i', payload[2:6])[0] # берутся 2,3,4,5 байты
+        Gy      = struct.unpack('<i', payload[6:10])[0]
+        Gz      = struct.unpack('<i', payload[10:14])[0]
+        Ax      = struct.unpack('<i', payload[14:18])[0]
+        Ay      = struct.unpack('<i', payload[18:22])[0]
+        Az      = struct.unpack('<i', payload[22:26])[0]
+        TC      = struct.unpack('<h', payload[26:28])[0]
+        Counter = payload[28] # беззнаковое
+        Status  = payload[29] # беззнаковое
+        CRC     = struct.unpack('<H', payload[29:31])[0]
+        checkCRC = crc16(payload[:-2]) # все кроме CRC
+
+
+        print(f"[{header}] | [{Gx:.3f}] [{Gy:.3f}] [{Gz:.3f}] | [{Ax:.3f}] [{Ay:.3f}] [{Az:.3f}] | [{TC}] | [{Counter}] [{Status}] | [{CRC}]  ")
+
+
+
+class SerialProtocol(asyncio.Protocol): # асинхронный протокол
+    def __init__(self, file):
+        self.buffer = b''
+        self.file = file
+
+        self.prev_counter = None
+        self.loss_count = 0
+        self.total_packets = 0
+
+        self.timestamps = []
+
+        self.last_stat_time = time.time()
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print("[v] Connected")
+
+    def data_received(self, data): # Важно переписать под структуру протокола
+        self.buffer += data
+
+        while True: # !!! бесконечный цикл
+            idx = self.buffer.find(SYNC)
+            if idx == -1:
+                self.buffer = self.buffer[-2:]
+                break
+
+            if len(self.buffer) < idx + FRAME_SIZE:
+                break
+
+            frame = self.buffer[idx:idx + FRAME_SIZE]
+            self.buffer = self.buffer[idx + FRAME_SIZE:]
+
+            #payload = frame[2:34] # получение пакета (его вырез, но в нашем случае можно не обрезать
+            payload = frame
+            # Распаковка пакета по протоколу
+            header      = struct.unpack('<H', payload[:2])[0] # не уверен с < или >
+            header_alt  = struct.unpack('<H', payload[:2])
+            Gx          = struct.unpack('<f', payload[3:6])
+            Gy          = struct.unpack('<f', payload[3:6])
+            Gz          = struct.unpack('<f', payload[3:6])
+            Ax          = struct.unpack('<f', payload[3:6])
+            Ay          = struct.unpack('<f', payload[3:6])
+            Az          = struct.unpack('<f', payload[3:6])
+            TC          = struct.unpack('<i', payload[26:])
+            Counter     = struct.unpack('<f', payload[30:32])
+            Status      = struct.unpack('<f', payload[3:6])
+            CRC         = struct.unpack('<f', payload[30:32])
+
+            if self.prev_counter is not None:
+                if counter != self.prev_counter + 1:
+                    self.loss_count += (counter - self.prev_counter - 1)
+
+            self.prev_counter = counter
+            self.total_packets += 1
+
+            ts = time.time_ns()
+            self.timestamps.append(ts)
+
+            self.file.write(struct.pack('<Q', ts))
+            self.file.write(payload)
+
+        self.print_stats()
+
+    def print_stats(self):
+        now = time.time()
+
+        if now - self.last_stat_time >= 1:
+            if len(self.timestamps) > 1:
+                dt = (self.timestamps[-1] - self.timestamps[0]) / 1e9
+                rate = len(self.timestamps) / dt if dt > 0 else 0
+            else:
+                rate = 0
+
+            print(f"Rate: {rate:.0f} pkt/s | Loss: {self.loss_count}")
+
+            self.timestamps.clear()
+            self.last_stat_time = now
+
+
+async def main():
+    loop = asyncio.get_running_loop()
+
+    with open("data_async.bin", "wb") as f:
+        transport, protocol = await serial_asyncio.create_serial_connection(
+            loop,
+            lambda: SerialProtocol(f),
+            PORT,
+            baudrate=BAUDRATE
+        )
+
+        try:
+            await asyncio.Future()  # run forever
+        finally:
+            transport.close()
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
